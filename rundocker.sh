66d322969e29ccf8d646ef2b7338908554e727c7
cryptout <cryptout@corputty.net>
2016-12-30 09:21:58 +0100

    fixed rundocker script

--- rundocker.sh
+++ rundocker.sh
@@ -4,19 +4,22 @@
 VOL_DATA="/Volumes/shares/docker/data/sickrage/data"
 VOL_CONFIG="/Volumes/shares/docker/data/sickrage/config"
 VOL_CACHE="/Volumes/shares/docker/cache/sickrage/cache"
+VOL_MEDIA="/Volumes/shares/docker/media"
 
 test -d ${VOL_DATA} || VOL_DATA="${PWD}${VOL_DATA}" && mkdir -p ${VOL_DATA}
 test -d ${VOL_CONFIG} || VOL_CONFIG="${PWD}${VOL_CONFIG}" && mkdir -p ${VOL_CONFIG}
 test -d ${VOL_CACHE} || VOL_CACHE="${PWD}${VOL_CACHE}" && mkdir -p ${VOL_CACHE}
+test -d ${VOL_MEDIA} || VOL_MEDIA="${PWD}${VOL_MEDIA}" && mkdir -p ${VOL_MEDIA}
 
 docker run -d -h $(hostname) \
     -p 8081:8081 \
     -v ${VOL_DATA}:/data \
     -v ${VOL_CONFIG}:/config \
     -v ${VOL_CACHE}:/cache \
+    -v ${VOL_MEDIA}:/media \
     -v /etc/localtime:/etc/timezone \
-    -e PUID=1000 \
-    -e PGID=1000 \
+    -e PUID=10000 \
+    -e PGID=10000 \
     --name=sickrage --restart=always cryptout/sickrage
 
   # for troubleshooting run
